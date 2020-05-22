2dd7949febc0537ea450893ca8a560fd56d9ffde
Marvin Gazibarić <m.gazibaric@live.de>
2020-05-22 16:52:43 +0200

    Implement basic hello world functionality

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 2 {
+            return Err("Please provide a file name.");
+        }
+
+        let filename = args[1].clone();
+
+        Ok(Config { filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("Source code: \r\n{}", contents);
+
+    Ok(())
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::process;
+
+use necromancer::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Executing file {}", config.filename);
+
+    if let Err(e) = necromancer::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
 }
+
