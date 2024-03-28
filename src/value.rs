11383feff46aa2ff10d0b4daf0e04c840bcd5f18
Marvin Gazibarić <m.gazibaric@live.de>
2024-03-28 18:41:49 +0100

    A̴̼ͣͯͯd͘d̜̲̥͖̔ͧ͋̏ č̸̶̻͙̹̦̊̋̂ͮ̎̊͋͂͘͠͞u̢͎͍͌̾͌ͥͣ̔ͮͭͮ͟͟r̡͙̝͇̘̼͇ͤ̆ͯ̈́ͮͪ͜͞ͅs̗̱̘͈̓͊̓ͥ͊̑̅̍̃e̙̜͔̩ͨ͑̄ͩ̚͝d̡͚͚͍ͬ̑̑ͬͮ͆̀̌ͅ v̶̤̀͛̕a͈ͣ̒l̵̨̞̟̺̉ͥ͒ͪͬ͊͊ͨ̕͜u̯̼̳̲͚̦̮̗̜ͭ́͐ͪͨͫͬ̆ͧ͜͝e̷̢̼͔͕̹̟̥͎̗̜͆̂̊͐ͫ̂̆ͤs̴̸̨̜̳̠͎̳̘͕̥̅̎ͤ͌̈̈͒͋͡

--- src/value.rs
+++ src/value.rs
@@ -2,17 +2,40 @@ use std::fmt::{Display, Formatter, Result};
 use std::iter::repeat_with;
 use std::ops::{Add, Div, Neg};
 
+use zalgo::{Generator, GeneratorArgs, ZalgoSize};
+
+/// A value that an entity can remember.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Value {
     Integer(i64),
     String(String),
     Boolean(bool),
+    Evil(String),
     Void,
 }
 
 impl Value {
-    fn random() -> Value {
-        Value::String(repeat_with(fastrand::alphanumeric).take(7).collect())
+    /// Generate a corrupted value.
+    fn corrupted() -> Value {
+        let text: String = repeat_with(fastrand::alphanumeric)
+            .take(fastrand::usize(7..=13))
+            .collect();
+        Value::Evil(Self::curse(&text))
+    }
+
+    /// Corrupt the value and make it evil.
+    fn corrupt(self: &Value) -> Value {
+        Value::Evil(Self::curse(&self.to_string()))
+    }
+
+    /// Curse the text with zalgo.
+    #[inline]
+    fn curse(text: &str) -> String {
+        let mut generator = Generator::new();
+        let mut out = String::new();
+        let args = GeneratorArgs::new(true, true, true, ZalgoSize::Maxi);
+        generator.gen(text, &mut out, &args);
+        out
     }
 }
 
@@ -31,17 +54,29 @@ impl PartialEq<Value> for &Value {
 impl<'a> Add<&'a Value> for Value {
     type Output = Value;
 
+    /// The `+` operator for the `Value` type.
+    ///
+    /// Performs type inference on a best-effort basis.
+    /// Returns some™ value if addition cannot be performed or would overflow.
     fn add(self, other: &Value) -> Value {
         match (self, other) {
-            (Value::Integer(i1), Value::Integer(i2)) => Value::Integer(i1 + i2),
+            (Value::Integer(i1), Value::Integer(i2)) => {
+                if let Some(sum) = i1.checked_add(*i2) {
+                    Value::Integer(sum)
+                } else {
+                    Value::corrupted()
+                }
+            }
             (Value::String(s1), Value::String(s2)) => Value::String(s1 + s2),
             (Value::String(s), Value::Integer(i)) => Value::String(format!("{}{}", s, i)),
             (Value::String(s), Value::Boolean(b)) => Value::String(format!("{}{}", s, b)),
             (Value::Integer(i), Value::String(s)) => Value::String(format!("{}{}", i, s)),
             (Value::Boolean(b), Value::String(s)) => Value::String(format!("{}{}", b, s)),
+            (Value::Evil(e), v) => Value::Evil(format!("{}{}", e, v.corrupt())),
+            (v, Value::Evil(e)) => Value::Evil(format!("{}{}", e, v.corrupt())),
             (Value::Void, v) => Value::from(v),
             (v, Value::Void) => Value::from(v),
-            _ => Value::random(),
+            _ => Value::corrupted(),
         }
     }
 }
@@ -49,12 +84,22 @@ impl<'a> Add<&'a Value> for Value {
 impl<'a, 'b> Div<&'b Value> for &'a Value {
     type Output = Value;
 
+    /// The `/` operator for the `Value` type.
+    ///
+    /// Performs type inference on a best-effort basis.
+    /// Returns some™ value if division cannot be performed or would overflow.
     fn div(self, other: &Value) -> Value {
         match (self, other) {
-            (Value::Integer(i1), Value::Integer(i2)) => Value::Integer(i1 / i2),
+            (Value::Integer(i1), Value::Integer(i2)) => {
+                if let Some(div) = i1.checked_div(*i2) {
+                    Value::Integer(div)
+                } else {
+                    Value::corrupted()
+                }
+            }
             (Value::Void, v) => Value::from(v),
             (v, Value::Void) => Value::from(v),
-            _ => Value::random(),
+            _ => Value::corrupted(),
         }
     }
 }
@@ -62,11 +107,21 @@ impl<'a, 'b> Div<&'b Value> for &'a Value {
 impl<'a> Neg for &'a Value {
     type Output = Value;
 
+    /// The unary `-` operator for the `Value` type.
+    ///
+    /// Performs type inference on a best-effort basis.
+    /// Returns some™ value if negation cannot be performed or would overflow.
     fn neg(self) -> Value {
         match self {
-            Value::Integer(i) => Value::Integer(-i),
+            Value::Integer(i) => {
+                if let Some(neg) = i.checked_neg() {
+                    Value::Integer(neg)
+                } else {
+                    Value::corrupted()
+                }
+            }
             Value::Void => Value::Void,
-            _ => Value::random(),
+            _ => Value::corrupted(),
         }
     }
 }
@@ -77,6 +132,7 @@ impl From<&Value> for Value {
             Value::Integer(i) => Value::Integer(*i),
             Value::String(s) => Value::String(String::from(s)),
             Value::Boolean(b) => Value::Boolean(*b),
+            Value::Evil(e) => Value::Evil(String::from(e)),
             Value::Void => Value::Void,
         }
     }
@@ -118,7 +174,8 @@ impl Display for Value {
             Value::Integer(i) => write!(fmt, "{}", i),
             Value::String(s) => write!(fmt, "{}", s),
             Value::Boolean(b) => write!(fmt, "{}", b),
-            Value::Void => write!(fmt, "The Void."),
+            Value::Evil(e) => write!(fmt, "{}", e),
+            Value::Void => Ok(()),
         }
     }
 }
